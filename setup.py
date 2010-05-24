ffc520eaa22915246b7348173b469f27c4d09565
Patrick Altman <paltman@gmail.com>
2010-05-23 21:31:39 -0500

    added proper setup.py script

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='django-pdf',
+      version='1.0.0',
+      description='A Django app for managing and processing PDF documents.',
+      author='Patrick Altman',
+      author_email='paltman@gmail.com',
+      url='http://github.com/paltman/django-pdf/',
+      packages=find_packages(),
+      classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ],
+    include_package_data = True,
+    zip_safe = False,
+    install_requires = ['django', 'celery', 'boto', 'simplejson']
+)
